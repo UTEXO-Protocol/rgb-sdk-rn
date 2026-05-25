f5f6d7428bdf4cc879ff2c342907ebb25e10c1ff
Yurii Bandrivskiy <bandrivskiy96@gmail.com>
2026-05-25 11:14:20 +0300

    fix: utexo network map

--- src/wallet/wallet-manager.ts
+++ src/wallet/wallet-manager.ts
@@ -1,8 +1,9 @@
 import { generateKeys as coreGenerateKeys } from '@utexo/rgb-sdk-core';
 import type { GeneratedKeys } from '@utexo/rgb-sdk-core';
+import { toNativeNetwork, BitcoinNetwork } from '../binding/Interfaces';
 
 export const createWallet = async (
   network: string = 'regtest'
 ): Promise<GeneratedKeys> => {
-  return coreGenerateKeys(network);
+  return coreGenerateKeys(toNativeNetwork(network as BitcoinNetwork));
 };
